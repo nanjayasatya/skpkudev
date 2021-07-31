f44b6904d612f0f01a10006904214d9ca0c1e3f7
Nanjaya Satya <nanjayaevan@gmail.com>
2021-08-01 02:55:04 +0800

    Pindah lokasi favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,34 +17,34 @@
     <link href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link href="https://cdn.datatables.net/rowreorder/1.2.7/css/rowReorder.dataTables.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.dataTables.min.css" rel="stylesheet" crossorigin="anonymous" />
-    
+
     <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.27.0/feather.min.js" crossorigin="anonymous"></script>
-  
+
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css" />
     <!-- FAVICON -->
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://skpku.bemfkuwks.com/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://skpku.bemfkuwks.com/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://skpku.bemfkuwks.com/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://skpku.bemfkuwks.com/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="https://skpku.bemfkuwks.com/apple-touch-icon-60x60.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://skpku.bemfkuwks.com/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="https://skpku.bemfkuwks.com/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://skpku.bemfkuwks.com/apple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/favicon-196x196.png" sizes="196x196" />
-    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/favicon-128.png" sizes="128x128" />
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://skpku.bemfkuwks.com/assets/favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/assets/favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/assets/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/assets/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/assets/favicon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="https://skpku.bemfkuwks.com/assets/favicon/favicon-128.png" sizes="128x128" />
     <meta name="application-name" content="&nbsp;" />
     <meta name="msapplication-TileColor" content="#FFFFFF" />
-    <meta name="msapplication-TileImage" content="mstile-144x144.png" />
-    <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
-    <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
-    <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
-    <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+    <meta name="msapplication-TileImage" content="assets/favicon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="assets/favicon/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="assets/favicon/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="assets/favicon/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="assets/favicon/mstile-310x310.png" />
 
 </head>
 <!-- BODY -->
