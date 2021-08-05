ce7e7bfc23e86de611825f09ca03a5c580be778f
nanjayasatya <nanjayaevan@gmail.com>
2021-08-06 01:44:02 +0800

    Perbaikan flexibility issue pada card selamat datang

--- application/views/staffbem/index.php
+++ application/views/staffbem/index.php
@@ -19,7 +19,7 @@
         <!-- Main page content-->
         <div class="container mt-n10">
             <div class="row">
-                <div class="col-xxl-4 col-xl-12 mb-4">
+                <div class="col-xl-12 mb-4">
                     <div class="card h-100">
                         <div class="card-body h-100 d-flex flex-column justify-content-center py-5 py-xl-4">
                             <div class="row align-items-center">
