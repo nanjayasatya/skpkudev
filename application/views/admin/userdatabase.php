fea47fc09008b64d6e812e707a89d539a6d7164d
nanjayasatya <nanjayaevan@gmail.com>
2021-08-06 16:04:02 +0800

    Perubahan button pada userdatabase admin

--- application/views/admin/userdatabase.php
+++ application/views/admin/userdatabase.php
@@ -18,75 +18,76 @@
         </header>
         <!-- Main page content-->
         <div class="container mt-n10">
-            <div class="row">
-               <!--<div class="col-xxl-4 col-xl-12 mb-4">-->
-                    <?php if (validation_errors()) : ?>
-                        <div class="alert alert-danger" role="alert"><?= validation_errors(); ?></div>
-                    <?php endif; ?>
 
 
-                    <?= $this->session->flashdata('message'); ?>
+            <?php if (validation_errors()) : ?>
+                <div class="alert alert-danger" role="alert"><?= validation_errors(); ?></div>
+            <?php endif; ?>
 
-                    <div class="card h-100">
-                        <div class="card-body h-100 d-flex flex-column justify-content-center py-5 py-xl-4">
-                            <table id="userdatabase" class="table table-bordered table-hover dataTable" style="width:100%">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nama</th>
-                                        <th>NPM</th>
-                                        <th>Angkatan & Kelas</th>
-                                        <th>Email</th>
-                                        <!-- <th>Active</th> -->
-                                        <th>Action</th>
+            <?= $this->session->flashdata('message'); ?>
 
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php $i = 1; ?>
-                                    <?php foreach ($userDatabaseMahasiswa as $ud) : ?>
-                                        <tr>
-                                            <th scope="row"><?= $i ?></th>
-                                            <td><?= $ud['name']; ?></td>
-                                            <td><?= $ud['npm']; ?></td>
-                                            <td><?= $ud['angkatan'] . $ud['kelas']; ?></td>
-                                            <td><?= $ud['email']; ?></td>
-                                            <!--<td><?= $ud['is_active']; ?></td> -->
-                                            <td>
-                                                <!--<a href="<?= base_url('admin/useredit/') . $ud['id']; ?>" class="btn btn-danger">Data SKP A</a> -->
-                                                <!-- <a href="" class="btn btn-danger">Delete</a> -->
-                                                <a href="<?= base_url('admin/useredit/') . $ud['id']; ?>" class="btn btn-purple btn-icon-split">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-info-circle"></i>
-                                                    </span>
-                                                    <span class="text" style="width:130px">Data Lengkap</span>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <table id="userdatabase" class="table table-bordered table-hover dataTable" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>NPM</th>
+                                <th>Angkatan & Kelas</th>
+                                <th>Email</th>
+                                <!-- <th>Active</th> -->
+                                <th>Action</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $i = 1; ?>
+                            <?php foreach ($userDatabaseMahasiswa as $ud) : ?>
+                                <tr>
+                                    <th scope="row"><?= $i ?></th>
+                                    <td><?= $ud['name']; ?></td>
+                                    <td><?= $ud['npm']; ?></td>
+                                    <td><?= $ud['angkatan'] . $ud['kelas']; ?></td>
+                                    <td><?= $ud['email']; ?></td>
+                                    <!--<td><?= $ud['is_active']; ?></td> -->
+                                    <td>
+                                        <div class="text-center">
+                                            <button class="btn btn-primary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <i class="fas fa-ellipsis-v"></i>
+                                            </button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <a class="dropdown-item" href="<?= base_url('admin/useredit/') . $ud['id']; ?>">
+                                                    <div class="dropdown-item-icon">
+                                                        <i class="fas fa-edit"></i>
+                                                    </div>Data Lengkap
                                                 </a>
-                                                <br>
-                                                <a href="<?= base_url('admin/userpasswordreset/') . $ud['id']; ?>" class="btn btn-danger btn-icon-split mt-2">
-                                                    <span class="icon text-white-50">
-                                                        <i class="fas fa-exclamation"></i>
-                                                    </span>
-                                                    <span class="text" style="width:140px">Reset Password</span>
+                                                <a class="dropdown-item" href="<?= base_url('admin/userpasswordreset/') . $ud['id']; ?>">
+                                                    <div class="dropdown-item-icon">
+                                                        <i class="fas fa-key"></i>
+                                                    </div>Reset Password
                                                 </a>
-                                            </td>
-                                        </tr>
-                                        <?php $i++; ?>
-                                    <?php endforeach; ?>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="text-center mb-5">
-                            <a href="<?= base_url('admin/useradd') ?> " class="btn btn-lg btn-success btn-icon-split">
-                                <span class="icon text-white-10">
-                                    <i class="fas fa-plus"></i>
-                                </span>
-                                <span class="text" style="width:190px">Tambah User Baru</span>
-                            </a>
-                        </div>
-
-                    </div>
-
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <?php $i++; ?>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
                 </div>
+                <div class="text-center mb-5">
+                    <a href="<?= base_url('admin/useradd') ?> " class="btn btn-lg btn-success btn-icon-split">
+                        <span class="icon text-white-10">
+                            <i class="fas fa-plus"></i>
+                        </span>
+                        <span class="text" style="width:190px">Tambah User Baru</span>
+                    </a>
+                </div>
+
+            </div>
+
+
 
 
     </main>
